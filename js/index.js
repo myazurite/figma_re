5e17a1f22c4ca85c8d31748b8b5e2b789ce7813f
Myazurite <longnvh2110@gmail.com Ceasar.248>
2021-05-12 21:10:36 +0700

    remake 1

--- js/index.js
+++ js/index.js
@@ -1,35 +1,12 @@
-var elem = document.querySelector('.main-carousel');
+var elem = document.querySelector('.grid');
 var flkty = new Flickity( elem, {
     // options
     cellAlign: 'left',
     contain: true,
     draggable: true,
     imagesLoaded: true,
-    adaptiveHeight: true,
-    wrapAround: true,
+    wrapAround: false,
     pageDots: false,
     prevNextButtons: false,
 });
 
-var elem_2 = document.querySelector('.bookletNav');
-var flkty_2 = new Flickity( elem_2, {
-    // options
-    cellAlign: 'left',
-    contain: true,
-    pageDots: false,
-    wrapAround: true,
-
-});
-
-var elem_3 = document.querySelector('.banner2');
-var flkty_3 = new Flickity( elem_3, {
-    // options
-    cellAlign: 'left',
-    contain: true,
-    draggable: true,
-    imagesLoaded: true,
-    adaptiveHeight: true,
-    wrapAround: true,
-    pageDots: false,
-    prevNextButtons: false,
-});
