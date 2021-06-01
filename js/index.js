f87bcc0c1ed761fea7857b647370b738967f0e47
Myazurite <longnvh2110@gmail.com>
2021-06-02 00:59:46 +0700

    done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
-var elem = document.querySelector('.grid');
-var flkty = new Flickity( elem, {
+
+var booklet = document.querySelector('.bookletNav');
+var flkty_4 = new Flickity( booklet, {
     // options
-    cellAlign: 'left',
+    cellAlign: 'center',
     contain: true,
     draggable: true,
     imagesLoaded: true,
     wrapAround: false,
     pageDots: false,
     prevNextButtons: false,
+});
+
+$(window).resize(function(){
+    if ($(window).width() <= 992) {
+        $('.card-title').click(function(){
+            $('.card-text').removeClass('hidden')
+        })
+    } else {
+        $('.card-title').click(function(){
+            $('.card-text').addClass('hidden')
+        })
+    }
 });
 
